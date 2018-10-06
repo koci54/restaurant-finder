84914b608cefbebee11c6682165e6b4d8eb6e82e
koci54 <zen.petr@gmail.com>
2018-10-06 13:43:11 +0200

    dependencies installed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
       lang = document.querySelector('html').lang;
     else lang = 'en';
 
-    var js_file = document.createElement('script');
+    let js_file = document.createElement('script');
     js_file.type = 'text/javascript';
     js_file.src =
       'https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&key=AIzaSyBkPOD_JvZ2A5JFPDenEgozAnTTjPMOhNM&language=' +
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
-var map;
+let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -28,26 +28,26 @@ function initMap() {
     .then(plotMarkers);
 }
 
-var markers;
-var bounds;
+// let markers;
+// let bounds;
 
-function plotMarkers(m) {
-  markers = [];
-  bounds = new google.maps.LatLngBounds();
+// function plotMarkers(m) {
+//   markers = [];
+//   bounds = new google.maps.LatLngBounds();
 
-  m.forEach(function(marker) {
-    var position = new google.maps.LatLng(marker.lat, marker.lng);
+//   m.forEach(function(marker) {
+//     let position = new google.maps.LatLng(marker.lat, marker.lng);
 
-    markers.push(
-      new google.maps.Marker({
-        position: position,
-        map: map,
-        animation: google.maps.Animation.DROP
-      })
-    );
+//     markers.push(
+//       new google.maps.Marker({
+//         position: position,
+//         map: map,
+//         animation: google.maps.Animation.DROP
+//       })
+//     );
 
-    bounds.extend(position);
-  });
+//     bounds.extend(position);
+//   });
 
-  map.fitBounds(bounds);
-}
+//   map.fitBounds(bounds);
+// }

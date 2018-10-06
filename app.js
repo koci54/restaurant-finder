cd3603e4ae6d63cf6599a0980670d9c99c4e8d27
koci54 <zen.petr@gmail.com>
2018-10-06 19:20:57 +0200

    geolocation is working

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Regular map
 let map;
+let infoWindow;
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 40.0519752, lng: -76.314270999999 },
@@ -7,4 +9,37 @@ function initMap() {
     scrollwheel: false,
     zoomControl: true
   });
+
+  infoWindow = new google.maps.InfoWindow({ map: map });
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        map.setCenter(pos);
+      },
+      function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? 'Error: The Geolocation service failed.'
+      : "Error: Your browser doesn't support geolocation."
+  );
 }

1b9994f78e29c07d5d1338b6c1ce4c2f09660498
koci54 <zen.petr@gmail.com>
2018-10-06 23:19:55 +0200

    foreach to complete

--- app.js
+++ app.js
@@ -43,3 +43,161 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
       : "Error: Your browser doesn't support geolocation."
   );
 }
+
+const places = [
+  {
+    id: 1,
+    restaurantName: 'Steakhouse',
+    address: '13, Hradcanska st. Prague',
+    lat: 50.087617,
+    lng: 14.418274,
+    averageRating: 5,
+    ratings: [
+      {
+        stars: 5,
+        comment: 'Great! But not many veggie options.'
+      },
+      {
+        stars: 5,
+        comment: 'My favorite restaurant!'
+      }
+    ]
+  },
+  {
+    id: 2,
+    restaurantName: 'MeetBurger',
+    address: '12, long st. Prague',
+    lat: 50.087617,
+    lng: 14.419371,
+    averageRating: 5,
+    ratings: [
+      {
+        stars: 4,
+        comment: 'Great! But not many veggie options.'
+      },
+      {
+        stars: 5,
+        comment: 'My favorite restaurant!'
+      }
+    ]
+  },
+  {
+    id: 3,
+    restaurantName: 'Bakery Crusta',
+    address: '39 Rue des Petites Écuries, 75010 Paris',
+    lat: 50.0860663,
+    lng: 14.4187638,
+    averageRating: 4,
+    ratings: [
+      {
+        stars: 4,
+        comment: 'Great! But not many veggie options.'
+      },
+      {
+        stars: 4,
+        comment: 'My favorite restaurant!'
+      }
+    ]
+  },
+  {
+    id: 4,
+    restaurantName: 'Trdelnik',
+    address: '1 st, Prague',
+    lat: 50.086151,
+    lng: 14.416491,
+    averageRating: 4,
+    ratings: [
+      {
+        stars: 4,
+        comment: 'Great! But not many veggie options.'
+      },
+      {
+        stars: 4,
+        comment: 'My favorite restaurant!'
+      }
+    ]
+  },
+  {
+    id: 5,
+    restaurantName: 'Las Adelitas',
+    address: '9 st, Prague',
+    lat: 50.086367,
+    lng: 14.419425,
+    averageRating: 3,
+    ratings: [
+      {
+        stars: 3,
+        comment: 'Great! But not many veggie options.'
+      },
+      {
+        stars: 3,
+        comment: 'My favorite restaurant!'
+      }
+    ]
+  },
+  {
+    id: 6,
+    restaurantName: 'Karavell',
+    address: '39 Rue des Petites Écuries, 75010 Paris',
+    lat: 50.08532,
+    lng: 14.41982,
+    averageRating: 2,
+    ratings: [
+      {
+        stars: 2,
+        comment: 'Great! But not many veggie options.'
+      },
+      {
+        stars: 2,
+        comment: 'My favorite restaurant!'
+      }
+    ]
+  },
+  {
+    id: 7,
+    restaurantName: 'BrewCoff Cafe',
+    address: '39 st, Prague',
+    lat: 50.086205,
+    lng: 14.419432,
+    averageRating: 2,
+    ratings: [
+      {
+        stars: 2,
+        comment: 'Great! But not many veggie options.'
+      },
+      {
+        stars: 2,
+        comment: 'My favorite restaurant!'
+      }
+    ]
+  },
+  {
+    id: 8,
+    restaurantName: 'Little Eataly',
+    address: '4 st, Prague',
+    location: {
+      lat: 50.08541,
+      lng: 14.420918
+    },
+    averageRating: 2,
+    ratings: [
+      {
+        stars: 2,
+        comment: 'Great! But not many veggie options.'
+      },
+      {
+        stars: 2,
+        comment: 'My favorite restaurant!'
+      }
+    ]
+  }
+];
+
+console.log(places);
+
+function renderPlaces(restaurants) {
+  places.forEach(place => {
+    `<h1>${place.restaurantName}</h1>`;
+  });
+}
+console.log(renderPlaces(places));
